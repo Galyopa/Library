02ebcdb3d6c2192cab483df1e2c926bf879637b5
Galyopa <galyopa@gmail.com>
2020-10-03 23:10:01 +0300

    Add author edit and create

--- author/urls.py
+++ author/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
-from .views import AuthorViewById, AuthorViewAll, delete_author, AuthorCreateView
-
-# app_name = "authors"
+from .views import AuthorViewById, AuthorViewAll,AuthorDeleteView, AuthorCreateView, AuthorUpdateView
 
 urlpatterns = [
     path('<int:pk>/', AuthorViewById.as_view(), name='author_by_id_url'),
     path('', AuthorViewAll.as_view(), name='all_author'),
-    path("delete/<int:id>/", delete_author, name="delete_author"),
-    # path("update/<int:id>/", views.register, name="update"),
-    path("create/", AuthorCreateView.as_view(), name="create"),
+    path("delete/<int:pk>/", AuthorDeleteView.as_view(), name="delete_author"),
+    path("update/<int:pk>/", AuthorUpdateView.as_view(), name="update_author"),
+    path("create/", AuthorCreateView.as_view(), name="create_author"),
 ]

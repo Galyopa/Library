eb5f7d71546899e58e2223238658f8cfc71b1f44
Galyopa <galyopa@gmail.com>
2020-10-04 19:20:48 +0300

    add login_required

--- book/urls.py
+++ book/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from .views import BooksListViewByAuthor, BooksViewById, BooksViewAll
 from . import views
@@ -6,7 +7,7 @@ urlpatterns = [
     path('authors_book/<authors>/', BooksListViewByAuthor.as_view(), name='books_list_url'),
     path('<int:pk>/', BooksViewById.as_view(), name='book_by_id_url'),
     path('', BooksViewAll.as_view(), name='all_books'),
-    path('create', views.create_book, name="book_create"),
-    path('update', views.update_book, name="book_update"),
-    path('delete', views.delete_book, name="book_delete")
+    path('create', login_required(views.create_book), name="book_create"),
+    path('update', login_required(views.update_book), name="book_update"),
+    path('delete', login_required(views.delete_book), name="book_delete")
 ]
